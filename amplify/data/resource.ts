a56b729ed5c4e7d4ad5ae207199243eb504fc9d4
Satyajit-Pawar <satyajitpawar2003@gmail.com>
2025-07-10 10:26:43 +0530

    changes for amplify

--- amplify/data/resource.ts
+++ amplify/data/resource.ts
@@ -1,18 +1,17 @@
 import { type ClientSchema, a, defineData } from '@aws-amplify/backend';
-
-const schema = a.schema({
-  Todo: a
+const schema = a.schema({  
+Note: a
     .model({
-      content: a.string(),
+      name:a.string(),
+      description: a.string(),
+      image: a.string(),
     })
-    .authorization((allow) => [allow.authenticated()]), // ✅ FIXED
+    .authorization((allow) => [allow.owner()]),
 });
-
 export type Schema = ClientSchema<typeof schema>;
-
 export const data = defineData({
   schema,
   authorizationModes: {
-    defaultAuthorizationMode: 'identityPool', // uses Cognito Identity Pool
+    defaultAuthorizationMode: 'userPool',
   },
 });
